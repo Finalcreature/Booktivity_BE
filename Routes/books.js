ac6a1308b04b32a800f9206cf302561b3efff01a
Finalcreature <finalnadav@gmail.com>
2023-01-25 15:34:06 +0200

    can add books - fixed user validation

--- Routes/books.js
+++ Routes/books.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const verifyAuth = require("../middlewares/verifyAuth");
 const bookController = require("../controllers/bookController");
+const Book = require("../MongoSchemas/BookSchema");
 
 const router = express.Router();
 
 router.get("/", verifyAuth, bookController.findBooks);
 
 router.get("/:id", verifyAuth, bookController.getBookInfo);
+
+router.post("/create", (req, res) => {
+  const newBook = new Book(req.body);
+  console.log(newBook);
+  newBook.save();
+  res.send(newBook);
+});
+
+router.put("/addQ", (req,res)=>[
+    
+])
 
 module.exports = router;
