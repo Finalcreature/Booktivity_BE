10b02c3862b9f68d2b58bf02e352679f20cdaf4b
Finalcreature <finalnadav@gmail.com>
2023-01-25 14:58:19 +0200

    Can add questions to db

--- MongoSchemas/QuestionSchema.js
+++ MongoSchemas/QuestionSchema.js
@@ -5,56 +5,48 @@ const questionSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  answers: [
-    {
-      question1: {
-        text: {
-          type: String,
-          required: true,
-        },
-        isCorrect: {
-          type: Boolean,
-          required: true,
-        },
+  answers: {
+    question1: {
+      text: {
+        type: String,
+        required: true,
+      },
+      isCorrect: {
+        type: Boolean,
+        required: true,
       },
     },
-    {
-      question2: {
-        text: {
-          type: String,
-          required: true,
-        },
-        isCorrect: {
-          type: Boolean,
-          required: true,
-        },
+    question2: {
+      text: {
+        type: String,
+        required: true,
+      },
+      isCorrect: {
+        type: Boolean,
+        required: true,
       },
     },
-    {
-      question3: {
-        text: {
-          type: String,
-          required: true,
-        },
-        isCorrect: {
-          type: Boolean,
-          required: true,
-        },
+    question3: {
+      text: {
+        type: String,
+        required: true,
+      },
+      isCorrect: {
+        type: Boolean,
+        required: true,
       },
     },
-    {
-      question4: {
-        text: {
-          type: String,
-          required: true,
-        },
-        isCorrect: {
-          type: Boolean,
-          required: true,
-        },
+    question4: {
+      text: {
+        type: String,
+        required: true,
+      },
+      isCorrect: {
+        type: Boolean,
+        required: true,
       },
     },
-  ],
+  },
 });
 
 module.exports = mongoose.model("question", questionSchema);
--- Routes/questions.js
+++ Routes/questions.js
@@ -1,24 +1,11 @@
 const express = require("express");
-const verifyAuth = require("../middlewares/verifyAuth");
+// const verifyAuth = require("../middlewares/verifyAuth");
 const Questions = require("../MongoSchemas/QuestionSchema");
 const router = express.Router();
 
 router.post("/", (req, res) => {
-  console.log("HERE");
-  console.log(req.body);
-  const question = new Questions(req.body);
-  res.send(question.validateSync().message);
-  return;
-  //   Questions.validate(req.body)
-  //     .then(() => {
-  //       Questions.create(req.body);
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //       res.send(err.message);
-  //     });
+  const newQuestion = Questions.create(req.body);
+  res.send("New Question added");
 });
-
-// router.get("/:id", );
 
 module.exports = router;
