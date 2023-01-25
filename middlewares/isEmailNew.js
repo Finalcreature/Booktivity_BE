3189e4a9eeaf24de0d879957014ba3d31304315f
Naël Achkar <naelachkar@hotmail.fr>
2023-01-25 18:20:47 +0200

    Fixing isEmailNew middlware

--- middlewares/isEmailNew.js
+++ middlewares/isEmailNew.js
@@ -1,7 +1,10 @@
 const UserSchema = require("../MongoSchemas/UserSchema");
 
 async function isEmailNew(req, res, next) {
-  const user = await UserSchema.find({ email: req.body.email });
+  if (req.body.email === "") {
+    res.status(400).send("Please enter a valid email address");
+  }
+  const user = await UserSchema.findOne({ email: req.body.email });
   if (user) {
     res.status(400).send("Email already used");
     return;
